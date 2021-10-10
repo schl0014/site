a7874f75414fe8ac765f55d0925ac122edab78e6
schl0014 <schl0014@hz.nl>
2021-10-10 20:55:50 +0200

    update

--- js/racing.js
+++ js/racing.js
@@ -10,9 +10,9 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {
     slideIndex = 1;
   }
